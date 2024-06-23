67eafca0ae9208b859c5818b30ee39b69e10ae0c
AmadeusK525 <amadeusk525@protonmail.com>
2024-06-23 17:20:05 -0300

    fix(build): fix generation of compile_commands.json file and remove from git

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -6,5 +6,6 @@ BUILD_TYPE=$1
 BUILD_DIR=build/$1
 
 cmake -G "Unix Makefiles" . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cp $BUILD_DIR/compile_commands.json compile_commands.json
 cmake --build $BUILD_DIR
 open $BUILD_DIR/BTFL-Launcher.app
