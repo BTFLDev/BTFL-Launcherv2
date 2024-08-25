201846754178c89120a92d3137ec676f3e524dd8
AmadeusK525 <amadeusk525@protonmail.com>
2024-08-24 23:55:56 -0300

    fix(script): directly run executable from Terminal to see log output

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -8,4 +8,4 @@ BUILD_DIR=build/$1
 cmake -G "Unix Makefiles" . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cp $BUILD_DIR/compile_commands.json compile_commands.json
 cmake --build $BUILD_DIR
-open $BUILD_DIR/BTFL-Launcher.app
+$BUILD_DIR/BTFL-Launcher.app/Contents/MacOS/BTFL-Launcher
